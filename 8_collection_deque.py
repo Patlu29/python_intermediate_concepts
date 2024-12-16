8ca10af25169e9c6440e853e79b4b84a1b16dbd7
PRAKASH RAJAN <prakashrajan2908@gmail.com>
2024-12-16 11:59:45 +0530

    pop method added

--- 8_collection_deque.py
+++ 8_collection_deque.py
@@ -8,5 +8,8 @@ print(d)
 
 d.append(4) # append in the end of the list 
 d.appendleft(5) # append in the left end of the list 
+print(d)
 
+d.pop() # remove or pop the last element of the list or which index we want to remove
+d.popleft() # removes the first element on the list 
 print(d)
